9360ea5196fe5cd894604d8e5bc9d9bfb4b67b63
ubuntu <ubuntu@ubuntu2404>
2026-07-02 11:10:31 -0400

    added file error macro

--- messages.h
+++ messages.h
@@ -1,4 +1,9 @@
-#define ERROR (s)
+#define ASM_ERROR(line_num, args) \
+    do { \
+        printf("Error in line %d: ", line_num); \
+        printf args; \
+        printf("\n"); \
+    } while(0)
 
 
 #define NO_ARGUMENTS_PASSED "Please pass at least one argument for assembler."
