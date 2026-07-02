d1077cc1484e7ffeec685f97f2a6710d395e81de
ubuntu <ubuntu@ubuntu2404>
2026-07-02 10:40:48 -0400

    adding error messages and file mane validations

--- assembler.c
+++ assembler.c
@@ -1,10 +1,12 @@
-
+#include "messages.h"
 
 
 
 
 int main(int argc, char *argv[]){
-
+	if (argc < 2){
+		printf()
+	}
 
 	return 0;
 }
---
+++ messages.h
@@ -0,0 +1,5 @@
+#define ERROR (s)
+
+
+#define NO_ARGUMENTS_PASSED "Please pass at least one argument for assembler."
+#define WRONG_FILE_EXTENTION "You may only pass files with .as to the assembler, please rename your file."
