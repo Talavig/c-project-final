bab69b6f0ced442922ed4e9bf89421e467a6e95b
ubuntu <ubuntu@ubuntu2404>
2026-08-08 15:22:26 -0400

    added docs to the macro table

--- macro_table.c
+++ macro_table.c
@@ -7,23 +7,29 @@
 #include "messages.h"
 
 MacroTableNode* findMacro(MacroTable macro_table, const char *name) {
-	MacroTableNode *current = macro_table;
+	MacroTableNode *current = macro_table; /* a pointer to the current node in the table*/
+
+	/*iterate over the linked list and return a node where a name match is found */
 	while (current != NULL) {
 		if (strcmp(current->macro_table_entry.name, name) == 0) {
 			return current;
 		}
 		current = current->next_entry;
 	}
+	/*otherwise, return null*/
 	return NULL;
 }
 
 Status addMacroToTable(MacroTable *macro_table, const char *name) {
-	MacroTableNode *new_node = (MacroTableNode*)malloc(sizeof(MacroTableNode));
+	MacroTableNode *new_node = (MacroTableNode*)malloc(sizeof(MacroTableNode)); /* the new node to be added*/
+
+	/*check if memory allocation went smoothly	 */
 	if (new_node == NULL) {
 		fprintf(stderr, ERR_MEM_ALLOC_FAILED, "macro table node");
 		return FAILURE;
 	}
 
+	/*allocate a string for the macro's name and check if it went well */
 	new_node->macro_table_entry.name = (char*)malloc(strlen(name) + 1);
 	if (new_node->macro_table_entry.name == NULL) {
 		fprintf(stderr, ERR_MEM_ALLOC_FAILED, "macro table entry name");
@@ -31,33 +37,44 @@ Status addMacroToTable(MacroTable *macro_table, const char *name) {
 		return FAILURE;
 	}
 
+	/* copy the name to the new node and set its content to null*/
 	strcpy(new_node->macro_table_entry.name, name);
 	new_node->macro_table_entry.content = NULL;
+
+	/* set the new node's next entry to be the original list head, and move the head to the new node*/
 	new_node->next_entry = *macro_table;
 	*macro_table = new_node;
 	return SUCCESS;
 }
 
 Status addLineToMacro(MacroTableNode *macro_node, const char *line) {
-	char *temp_content;
+	char *temp_content; /* pointer for content allocation safety*/
+
+	/* if one of the pointers is null, break out*/
 	if (macro_node == NULL || line == NULL) {
 		return FAILURE;
 	}
 
+	/* case 1: the content is empty, so we malloc a new buffer for it*/
 	if (macro_node->macro_table_entry.content == NULL) {
+		/* allocate the content's size +1 for a null terminator*/
 		macro_node->macro_table_entry.content = (char*)malloc(strlen(line) + 1);
 		if (macro_node->macro_table_entry.content == NULL) {
 			fprintf(stderr, ERR_MEM_ALLOC_FAILED, "macro entry content initialization");
 			return FAILURE;
 		}
+		/* copy content to buffer*/
 		strcpy(macro_node->macro_table_entry.content, line);
 	}
+	/* case 2: the content hase some lines, and we realloc it in order to add to it*/
 	else{
+		/* reallocate memory to the size of the previous content + the new content + null terminator*/
 		temp_content = (char*)realloc(macro_node->macro_table_entry.content, strlen(macro_node->macro_table_entry.content) + strlen(line) + 1);
 		if (temp_content == NULL) {
 			fprintf(stderr, ERR_MEM_ALLOC_FAILED, "macro entry content addition");
 			return FAILURE;
 		}
+		/* concatenate content to buffer*/
 		macro_node->macro_table_entry.content = temp_content;
 		strcat(macro_node->macro_table_entry.content, line);
 	}
@@ -65,10 +82,13 @@ Status addLineToMacro(MacroTableNode *macro_node, const char *line) {
 }
 
 void freeMacroTable(MacroTable *macro_table) {
-	MacroTableNode *current = *macro_table;
-	MacroTableNode *next_node;
+	MacroTableNode *current = *macro_table; /* the current node we are freeing*/
+	MacroTableNode *next_node; /* a pointer to the next node in the linked list*/
 
+	/* iterate over all the nodes*/
 	while (current != NULL) {
+		/* store the pointer to the next item, free the current one and go to the next one*/
+		/* if there is a dynamically allocated content buffer, free it too*/
 		next_node = current->next_entry;
 		free(current->macro_table_entry.name);
 		if (current->macro_table_entry.content != NULL) {
@@ -78,6 +98,7 @@ void freeMacroTable(MacroTable *macro_table) {
 
 		current = next_node;
 	}
+	/* nullify the original table pointer*/
 	*macro_table = NULL;
 }
 
--- macro_table.h
+++ macro_table.h
@@ -3,30 +3,63 @@
 
 #include "consts.h"
 
+/*
+ * a struct to hold a single macro definition
+ * name: a dynamically allocated string holding the name of the macro
+ * content: a dynamically allocated string holding the content of the macro
+ */
 typedef struct {
 	char *name;
 	char *content;
 } MacroTableEntry;
 
+
+/*
+ * a struct to hold a single item in the macro  table linked list.
+ * macro_table_entry: the entry in the table
+ * next_entry: a pointer to the next entry
+ */
 typedef struct MacroTableNode {
 	MacroTableEntry macro_table_entry;
 	struct MacroTableNode *next_entry;
 } MacroTableNode;
 
+/*
+ * the macro table itself, which is a pointer to a linked list
+ */
 typedef MacroTableNode* MacroTable;
 
+/*
+ * search of a macro in the macro table by name (iterate over the linked list)
+ * macro_table: the table to look in
+ * name: the name of the macro to look for as specified in its name field
+ *
+ * return a pointer to the MacroTableNode if found, NULL if the macro does not exist
+ */
 MacroTableNode* findMacro(MacroTable macro_table, const char *name);
+
+/*
+ * add a new, empty entry to the start of the macro table
+ * macro_table: a pointer to the macro table (pointer to the head node)
+ * name: the name of the macro to be added
+ *
+ * return SUCCESS if the entry was added, FAILURE if addition failed
+ */
 Status addMacroToTable(MacroTable *macro_table, const char *name);
+
+/*
+ * append a new line of text to an existing macro's content. this is done using allocation/reallocation of the content memory.
+ * macro_node: the macro to add the line to
+ * line: the line to be added to the macros content
+ *
+ * return SUCCESS if the line was appended successfully, FAILURE on line addition issue
+ */
 Status addLineToMacro(MacroTableNode *macro_node, const char *line);
+
+/*
+ * frees all memory allocated for the symbol table by iterating over all of the tables nodes and set the main table pointer to null
+ * macro_table: a pointer to the macro table to be freed.
+ */
 void freeMacroTable(MacroTable *macro_table);
-
-
-
-
-
-
-
-
-
 
 #endif
