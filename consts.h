d12af4e6095dc56fff05c6ab9fea2ec97419957c
ubuntu <ubuntu@ubuntu2404>
2026-08-08 13:44:48 -0400

    comments on consts file

--- consts.h
+++ consts.h
@@ -4,20 +4,20 @@
 /*
  * machine related consts
  */
-#define NUM_REGISTERS 32
-#define REGISTER_BIT_SIZE 32
-#define MAX_MEMORY_SIZE (1 << 25)
-#define IC_INITIAL_VALUE 100
-#define DC_INITIAL_VALUE 0
-#define INSTRUCTION_BYTES_SIZE 4
+#define NUM_REGISTERS 32 /* registeer count in imaginary machine */
+#define REGISTER_BIT_SIZE 32 /* size of single register */
+#define MAX_MEMORY_SIZE (1 << 25) /* size of smemory in machine */
+#define IC_INITIAL_VALUE 100 /* starting value of the instruction counter as defined*/
+#define DC_INITIAL_VALUE 0 /* starting value of the data counter as defined*/
+#define INSTRUCTION_BYTES_SIZE 4 /* size in bytes of a single instrution*/
 
 /*
  * file and assembler limits related consts
  */
 #define MAX_LABEL_LENGTH 31
 #define MAX_TOKEN_LENGTH 32 /* 31 for max label + 1 for \0*/
-#define MAX_ASSEMBLY_LINE_COUNT 10000
-#define MAX_SINGLE_LINE_LENGTH 80
+#define MAX_ASSEMBLY_LINE_COUNT 10000 /* assumpion: a single file may not pass 10000 lines*/
+#define MAX_SINGLE_LINE_LENGTH 80 /*as defined by the manual*/
 #define MAX_OPERANDS_PER_LINE (MAX_SINGLE_LINE_LENGTH / 2) /* derived from line len / 2 for comma and value*/
 
 
@@ -25,7 +25,7 @@
  * file extentions
  */
 #define INPUT_ASSEMBLY_FILE ".as"
-#define FILE_EXTERNTION_LENGTH (sizeof(".as")/sizeof(char) - 1)
+#define FILE_EXTERNTION_LENGTH (sizeof(".as")/sizeof(char) - 1) /* comes down to 3, but written in this way for SOLID*/
 #define MACRO_ASSEMBLY_FILE ".am"
 #define OUTPUT_OBJECT_FILE ".ob"
 #define OUTPUT_EXTERNALS_FILE ".ext"
@@ -64,6 +64,7 @@
 
 /*
  * bit masks % shift consts
+ * all shift consts and masks are as described in the manual
  */
 
 #define OPCODE_SHIFT 26
@@ -90,7 +91,8 @@
 #define OUTPUT_SYMBOL_FORMAT "%s %04d\n"
 
 /*
- * instruction opcodes and functs
+ * instruction opcodes and functs, all as described in the manual
+ * these are implemented as enumes for later use in switch cases
  */
 
 #define R_INSTRUCTIONS_ARITHMATIC_OPCODES 0
@@ -98,6 +100,9 @@
 #define NON_R_FUNCT_VALUE 0
 
 
+/*
+ * holds the FUNCT values for arithmatic r instructions that share an opcode of 0
+ */
 typedef enum R_INSTRUCTIONS_ARITHMATIC_FUNCTS {
 	ADD=1,
 	SUB,
@@ -106,13 +111,18 @@ typedef enum R_INSTRUCTIONS_ARITHMATIC_FUNCTS {
 	NOR
 } R_INSTRUCTIONS_ARITHMATIC_FUNCTS;
 
+/*
+ * holds the FUNCT values for memory r instructions that share an opcode of 1
+ */
 typedef enum R_INSTRUCTIONS_MEMORY_FUNCTS {
 	MOVE=1,
 	MVHI,
 	MVLO
 } R_INSTRUCTIONS_MEMORY_FUNCTS;
 
-
+/*
+ * holds the OPCODE values for I instructions
+ */
 typedef enum I_INSTRUCTIONS_OPCODES {
 	ADDI=10,
 	SUBI,
@@ -131,6 +141,9 @@ typedef enum I_INSTRUCTIONS_OPCODES {
 	SH
 } I_INSTRUCTIONS_OPCODES;
 
+/*
+ * holds the OPCODE values for J instructions
+ */
 typedef enum J_INSTRUCTIONS_OPCODES {
 	JMP=30,
 	LA,
@@ -138,7 +151,10 @@ typedef enum J_INSTRUCTIONS_OPCODES {
 	HLT=63
 } J_INSTRUCTIONS_OPCODES;
 
-
+/*
+ * holds the different attributes each label can hase. the values are powers of 2,\
+ * as we use a bitmap in order to hold th labels total abbributes state
+ */
 typedef enum {
 	NONE = 0,
 	CODE = 1,
@@ -147,13 +163,23 @@ typedef enum {
 	EXTERNAL = 8
 } Attribute;
 
-
+/*
+ * holds the different instruction types values
+ */
 typedef enum {
 	R_TYPE,
 	I_TYPE,
 	J_TYPE
 } InstructionType;
 
+/*
+ * holds a single instruction:
+ * name: a string containing the canon name of the instruction
+ * type: the enum of the instruction type (r,i,j)
+ * opcode: instruction's opcode
+ * oprands: the number of oprands the instruction accepts
+ * funct: used for r types, otherwise 0
+ */
 typedef struct {
 	char *name;
 	InstructionType type;
@@ -162,6 +188,9 @@ typedef struct {
 	int funct;
 } Instruction;
 
+/*
+ * an array of akk instructions and its length, its actual value is computed in utils.c
+ */
 extern Instruction instructions[];
 extern const int NUM_INSTRUCTIONS;
 
@@ -169,11 +198,17 @@ extern const int NUM_INSTRUCTIONS;
  * control flow
  */
 
+/*
+ * an enum for boolean values
+ */
 typedef enum {
 	FALSE,
 	TRUE
 } Boolean;
 
+/*
+ * an enum for the status of different processes happening in the assembly process
+ */
 typedef enum {
 	FAILURE,
 	SUCCESS
