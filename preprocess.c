b9a8ba2957d35e667af16b5fa036e66795284ddf
ubuntu <ubuntu@ubuntu2404>
2026-08-08 10:05:34 -0400

    removing am file if preprocess failed

--- preprocess.c
+++ preprocess.c
@@ -141,8 +141,12 @@ Status preprocessScript(char * file_base_name){
 	fclose(input_file);
 	fclose(output_file);
 	free(input_file_name);
-	free(output_file_name);
 	freeMacroTable(macro_table);
+
+	if (run_status == FAILURE) {
+		remove(output_file_name);
+	}
+	free(output_file_name);
 
 	return run_status;
 }
