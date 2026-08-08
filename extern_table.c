5566f2532d64b338785cdbfcfc09ed80a072d777
ubuntu <ubuntu@ubuntu2404>
2026-08-08 14:56:10 -0400

    added docs to the extern table module

--- extern_table.c
+++ extern_table.c
@@ -7,27 +7,33 @@
 
 
 Status addEntryToExternTable(ExternTable *extern_table, ExternTableEntry new_entry){
+	/* allocate memory for new node and check if it went smoothly*/
 	ExternTableNode *new_node = (ExternTableNode*)malloc(sizeof(ExternTableNode));
 	if (new_node == NULL) {
 		fprintf(stderr, ERR_MEM_ALLOC_FAILED, "extern table node");
 		return FAILURE;
 	}
 
+	/* copy the entry's data to the new node*/
 	new_node->extern_table_entry = new_entry;
+
+	/* insert the node to the beginning of the list, and connect its start to the new node */
 	new_node->next_entry = *extern_table;
 	*extern_table = new_node;
 	return SUCCESS;
 }
 
 void freeExternTable(ExternTable *extern_table){
-	ExternTableNode *current = *extern_table;
-	ExternTableNode *next_node;
+	ExternTableNode *current = *extern_table; /* the current node we are freeing*/
+	ExternTableNode *next_node; /* the node after current*/
 
+	/*iterate over all nodes in the table*/
 	while (current != NULL){
+		/* store the pointer to the next item, free the current one and go to the next one*/
 		next_node = current->next_entry;
 		free(current);
 		current = next_node;
 	}
-
+	/* nullify the original table pointer*/
 	*extern_table = NULL;
 }
--- extern_table.h
+++ extern_table.h
@@ -4,21 +4,44 @@
 #include "consts.h"
 
 
-
+/*
+ * a struct to hold a single entry (label) in the assembler's extern table.
+ * symbol: a label (up to a label's length), as it appears in the asm code
+ * address: the address assigned to the label
+ */
 typedef struct ExternTableEntry {
 	char symbol[MAX_LABEL_LENGTH];
 	int address;
 } ExternTableEntry;
 
+/*
+ * a struct to hold a single item in the extern table linked list.
+ * extern_table_entry: the entry in the table
+ * next_entry: a pointer to the next entry
+ */
 typedef struct ExternTableNode{
 	ExternTableEntry extern_table_entry;
 	struct ExternTableNode* next_entry;
 } ExternTableNode;
 
+/*
+ * the exterm table itself, which is a pointer to a linked list
+ */
 typedef ExternTableNode* ExternTable;
 
-
+/*
+ * add a new extern entry to the start of the extern table
+ * extern_table: a pointer to the extern table (pointer to the head node)
+ * new_entry: the extern data to be added
+ *
+ * return SUCCESS if the entry was added, FAILURE if addition failed
+ */
 Status addEntryToExternTable(ExternTable *extern_table, ExternTableEntry new_entry);
+
+/*
+ * frees all memory allocated for the extern table by iterating over all of the tables nodes and set the main table pointer to null
+ * extern_table: a pointer to the extern table to be freed.
+ */
 void freeExternTable(ExternTable *extern_table);
 
 
